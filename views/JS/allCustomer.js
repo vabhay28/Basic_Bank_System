28a6a3b2a7841c8018d207ea8ee7ec1b69f347da
Abhay Vishwakarma <abhayv481@gmail.com>
2021-08-07 16:27:18 +0530

    Rendered data from local storage

--- views/JS/allCustomer.js
+++ views/JS/allCustomer.js
@@ -1,19 +1,66 @@
-let customer = {
-    account_no: "12021002",
-    name: "Abhay",
-    email: "abhay@gmail.com",
-    balance: "20000"
+const tableRow = document.getElementById("table-row")
+
+//let allCustomers = [];
+
+function getCustomer() {
+    tableRow.innerHTML = "";
+    for (let i = 0; i < custData.length; i++) {
+        let customer = JSON.parse(localStorage.getItem(custData[i].account_no));
+        renderData(customer);
+    }
+
 }
 
-let allCustomers = [customer]
-
-//localStorage.setItem("12021002", JSON.stringify(customer))
-
-for(let i = 12021001; i < 12021003; i++)
-{
-let customers = JSON.parse(localStorage.getItem(i));
-console.log(customers.name)
-console.log(customers.account_no)
-console.log(customers.email)
-console.log(customers.balance)
+function setCustomer() {
+    // let customer = {
+    //     account_no: "12021001",
+    //     name: "Abhay",
+    //     email: "abhay@gmail.com",
+    //     balance: "20000"
+    // }
+    // localStorage.setItem("12021001", JSON.stringify(customer));
+    // allCustomers = customer;
+    for (let i = 0; i < custData.length; i++) {
+    let customer = {
+        account_no: custData[i].account_no,
+        name: custData[i].name,
+        email: custData[i].email,
+        balance: custData[i].balance
+    }
+    localStorage.setItem(custData[i].account_no, JSON.stringify(customer));
 }
+}
+
+function renderData(customer){
+    let listCustomers = `
+        <tr>
+            <td>${customer.account_no}</td>
+            <td>${customer.name}</td>
+            <td>${customer.email}</td>
+            <td>${customer.balance}</td>
+            <td><button class="btn btn-info" id="${customer.account_no}">Details</Button></td>        
+        </tr>    
+        `;
+    tableRow.insertAdjacentHTML( 'beforeend', listCustomers );
+    // tableRow.appendChild(listCustomers);
+    // tableRow.innerHTML = listCustomers;
+}
+
+function btnDetails(){
+    for (let i = 0; i < custData.length; i++) {
+    let btnDetail = document.getElementById(`${custData[i].account_no}`);
+    btnDetail.onclick = function() {
+        console.log(custData[i].account_no);
+    };
+    }
+}
+
+
+function loader(){
+    setCustomer();
+    getCustomer();
+    btnDetails();
+}
+
+window.onload = loader;
+
---
+++ views/JS/custData.js
@@ -0,0 +1,76 @@
+let custData = [{
+    "account_no": "8416305161",
+    "name": "Rey Thomasset",
+    "email": "rthomasset0@walmart.com",
+    "balance": "8757.00"
+  }, {
+    "account_no": "0745051928",
+    "name": "Sofia Harrow",
+    "email": "sharrow1@pinterest.com",
+    "balance": "8311.54"
+  }, {
+    "account_no": "9641004190",
+    "name": "Troy Kinastan",
+    "email": "tkinastan2@histats.com",
+    "balance": "2324.79"
+  }, {
+    "account_no": "9784514362",
+    "name": "Nev Uttermare",
+    "email": "nuttermare3@prweb.com",
+    "balance": "8587.86"
+  }, {
+    "account_no": "3026504866",
+    "name": "Brandyn Rowden",
+    "email": "browden4@google.fr",
+    "balance": "6438.80"
+  }, {
+    "account_no": "3656041741",
+    "name": "Humbert Bewicke",
+    "email": "hbewicke5@seesaa.net",
+    "balance": "1052.84"
+  }, {
+    "account_no": "4357994632",
+    "name": "Pyotr Becerro",
+    "email": "pbecerro6@hc360.com",
+    "balance": "9952.50"
+  }, {
+    "account_no": "0715122916",
+    "name": "Olav Proudley",
+    "email": "oproudley7@prnewswire.com",
+    "balance": "9087.60"
+  }, {
+    "account_no": "5611890172",
+    "name": "Benyamin Beckingham",
+    "email": "bbeckingham8@free.fr",
+    "balance": "1251.32"
+  }, {
+    "account_no": "4854295384",
+    "name": "Jacob Goldman",
+    "email": "jgoldman9@noaa.gov",
+    "balance": "5680.26"
+  }, {
+    "account_no": "1402865627",
+    "name": "Leoline Campkin",
+    "email": "lcampkina@parallels.com",
+    "balance": "8048.57"
+  }, {
+    "account_no": "9730559872",
+    "name": "Ofilia Aujean",
+    "email": "oaujeanb@macromedia.com",
+    "balance": "7594.91"
+  }, {
+    "account_no": "9051882254",
+    "name": "Nester Huleatt",
+    "email": "nhuleattc@booking.com",
+    "balance": "7991.34"
+  }, {
+    "account_no": "4241885217",
+    "name": "Gelya Lockless",
+    "email": "glocklessd@miitbeian.gov.cn",
+    "balance": "5306.04"
+  }, {
+    "account_no": "7163967837",
+    "name": "Jerrie Checci",
+    "email": "jcheccie@upenn.edu",
+    "balance": "9137.88"
+  }];
